ce263f37c4b62ad869497156f4a6ce74d956be25
Filippos Bagordakis <64334902+FilipposB@users.noreply.github.com>
2024-12-29 19:10:59 +0200

    Fix bug on images

--- app/page/article.tsx
+++ app/page/article.tsx
@@ -1,18 +1,18 @@
 import Image from "next/image";
 import { IArticle } from "../model/article.model";
 
-export default function Article ({article}: {article: IArticle})  {
+export default function Article({ article }: { article: IArticle }) {
 
     return (
         <>
 
-        <div className="grid grid-cols-1 gap-2 ">
-        <div className="mb-4 text-4xl font-extrabold  leading-none tracking-tight text-gray-900 md:text-5xl lg:text-4xl dark:text-white">{article.title}</div>
-        <Image className="rounded-lg overflow-hidden place-self-center w-3/4 mb-4" src={article.google_image_url} alt={article.google_image_query}></Image>
-        <div className="whitespace-pre-line border-t pt-2">{article.article_body}</div>
-        <div className="italic mt-2 p-2 border-t pt-2">Author:  <span className="font-extrabold ">{article.author}</span></div>
-        </div>
-       
+            <div className="grid grid-cols-1 gap-2 ">
+                <div className="mb-4 text-4xl font-extrabold  leading-none tracking-tight text-gray-900 md:text-5xl lg:text-4xl dark:text-white">{article.title}</div>
+                <Image className="rounded-lg object-contain  overflow-hidden place-self-center mb-4" src={article.google_image_url} alt={article.google_image_query} width={500} height={500}/>
+                <div className="whitespace-pre-line border-t pt-2">{article.article_body}</div>
+                <div className="italic mt-2 p-2 border-t pt-2">Author:  <span className="font-extrabold ">{article.author}</span></div>
+            </div>
+
 
         </>
     );
